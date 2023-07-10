51f2ffdeece91a4e718079eef46f38a62d4b86d7
unknown <gregmasterbr@gmail.com>
2023-07-10 20:52:03 -0300

    Adicionando cenários de testes na home

--- eventex/settings.py
+++ eventex/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$kerw^19a+jc6lz%zm#889eavhb81(qe*bym!)zu5&yo1x+!7x"
+#SECRET_KEY = "django-insecure-$kerw^19a+jc6lz%zm#889eavhb81(qe*bym!)zu5&yo1x+!7x"
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
