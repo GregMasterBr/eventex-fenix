c5c1eb1fb98cbae26c391f2122b670c1d7d7184c
unknown <gregmasterbr@gmail.com>
2023-07-24 21:44:24 -0300

    Como sei quem se inscreveu?

--- eventex/settings.py
+++ eventex/settings.py
@@ -133,10 +133,11 @@ STATIC_ROOT = str(BASE_DIR / 'staticfiles')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-EMAIL_BACKEND= 'django.core.mail.backends.console.EmailBackend'
-#EMAIL_BACKEND=config('EMAIL_BACKEND')
-# EMAIL_HOST=config('EMAIL_HOST')
-# EMAIL_PORT=config('EMAIL_PORT',cast=int)
-# EMAIL_USE_TLS=config('EMAIL_USE_TLS', cast=bool)
-# EMAIL_HOST_USER=config('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD')
+#EMAIL_BACKEND= 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND=config('EMAIL_BACKEND')
+EMAIL_HOST=config('EMAIL_HOST')
+EMAIL_PORT=config('EMAIL_PORT',cast=int)
+EMAIL_USE_TLS=config('EMAIL_USE_TLS', cast=bool)
+EMAIL_HOST_USER=config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD=config('EMAIL_HOST_PASSWORD')
+PYTHONHTTPSVERIFY=0 
