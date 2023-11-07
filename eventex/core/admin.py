ce52ba6209f697e7e9e0d4967ad428e16ef85d51
unknown <gregmasterbr@gmail.com>
2023-11-06 21:07:39 -0300

    Exibindo os contatos EMAIL e TELEFONE, através da relação com o CONTACT. Este tipo de estratégia vaza muito informação.

--- eventex/core/admin.py
+++ eventex/core/admin.py
@@ -11,7 +11,7 @@ class ContactInLine(admin.TabularInline):
 class SpeakerModelAdmin(admin.ModelAdmin):
     inlines = [ContactInLine]
     prepopulated_fields = {'slug': ('name',)}
-    list_display = ['photo_img', 'name', 'website_link']
+    list_display = ['photo_img', 'name', 'website_link', 'email', 'phone']
     search_fields = ('name',)
 
     def website_link(self, obj):
@@ -26,7 +26,15 @@ class SpeakerModelAdmin(admin.ModelAdmin):
     photo_img.short_description = 'foto'    
 
 
+    def email(self, obj):
+        return Contact.objects.filter(kind=Contact.EMAIL, speaker=obj).first()
+        
+    email.short_description = 'e-mail'
 
+    def phone(self, obj):
+        return Contact.objects.filter(kind=Contact.PHONE, speaker=obj).first()
+
+    phone.short_description = 'telefone'
 
 admin.site.register(Speaker, SpeakerModelAdmin)
 admin.site.register(Talk)
