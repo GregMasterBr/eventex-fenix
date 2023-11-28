29db502cef1f49b8e6cd9bdef469300b7186128b
unknown <gregmasterbr@gmail.com>
2023-11-27 21:17:42 -0300

    Class Based View - Home - Parte 5 TemplateViewn - acúmula responsabilidades do templateResponseMixin e já lida com o método get.

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from eventex.core.models import Speaker, Talk, Course
-from django.views.generic import View 
+from django.views.generic import View, TemplateView
 from django.views.generic.base import TemplateResponseMixin
 
-class GenericHomeView(TemplateResponseMixin, View):
+class GenericHomeView(TemplateView):
     object_list = None
     context_object_name = None
-
-    def get(self, *args, **kwargs):
-        context = self.get_context_data()
-        return self.render_to_response(context)
-    
+  
     def get_context_data(self, **kwargs):
         context =  { self.context_object_name: self.object_list}
         context.update(kwargs)
