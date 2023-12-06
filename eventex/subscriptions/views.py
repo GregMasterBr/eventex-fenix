f7f6e2928a640c2c5c9b94740b480253442be159
unknown <gregmasterbr@gmail.com>
2023-12-06 19:52:01 -0300

    criando uma classe genérica para gerenciar a visualização da view com a generic view

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -7,33 +7,26 @@ from django.template.loader import render_to_string
 from django.contrib import messages
 from django.conf import settings
 from django.shortcuts import redirect, resolve_url as r
-from django.views.generic import DetailView
+from django.views.generic import DetailView, View
 
-def new(request):
-   if request.method == 'POST':
-      return create(request)
+class SubscriptionCreate(View):
+   def get(self, *args, **kwargs):
+      return render(self.request,'subscriptions/subscription_form.html', {'form':SubscriptionForm()}) 
+
+   def post(self, *args, **kwargs):
+      form = SubscriptionForm(self.request.POST)
+
+      if not form.is_valid():
+         return render(self.request,'subscriptions/subscription_form.html', {'form':form})
+
+      subscription = form.save() # quando o formulário for muito alinhado com a model.
+
+      #envia email
+      _send_mail('Confirmação de inscrição', settings.DEFAULT_FROM_EMAIL, subscription.email, 'subscriptions/subscription_email.txt', {'subscription':subscription})
+      
+      return HttpResponseRedirect(r('subscriptions:detail', subscription.pk))
    
-   return empty_form(request)
-
-
-def empty_form(request):      
-   return render(request,'subscriptions/subscription_form.html', {'form':SubscriptionForm()}) 
-
-
-def create(request):
-   form = SubscriptionForm(request.POST)
-
-   if not form.is_valid():
-      return render(request,'subscriptions/subscription_form.html', {'form':form})
-
-   #subscription = Subscription.objects.create(**form.cleaned_data)
-   subscription = form.save() # quando o formulário for muito alinhado com a model.
-
-   #envia email
-   _send_mail('Confirmação de inscrição', settings.DEFAULT_FROM_EMAIL, subscription.email, 'subscriptions/subscription_email.txt', {'subscription':subscription})
-   
-   return HttpResponseRedirect(r('subscriptions:detail', subscription.pk))
-   
+new = SubscriptionCreate.as_view()
 
 detail = DetailView.as_view(model=Subscription)
 
